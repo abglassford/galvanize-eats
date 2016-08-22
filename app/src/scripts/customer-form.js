75f288de9547d8e061c939b571e824a2597bcbd9
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-22 15:03:31 -0600

    add clear functionality after form is submitted

--- app/src/scripts/customer-form.js
+++ app/src/scripts/customer-form.js
@@ -9,5 +9,12 @@ $(`form`).on(`submit`, function (event) {
     url: 'https://galvanize-eats-api.herokuapp.com/orders'
   }).done(function(data){
     console.log(data);
+    $(`.info-form`).each(function(){
+    this.reset();
+    $(`.cart-list`).html(``)
+    $(`.subtotal`).text(`Subtotal:`)
+    $(`.tax`).text(`Tax:`)
+    $(`.grand-total`).text(`Grand Total:`)
+    })
   })
 })
