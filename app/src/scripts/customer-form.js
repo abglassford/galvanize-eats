d0c4e0761f896022f2006bbef2e4e5c4e5a0aa00
Alexander Glassford <a.b.glassford@gmail.com>
2016-08-22 14:31:59 -0600

    hit POST api

--- app/src/scripts/customer-form.js
+++ app/src/scripts/customer-form.js
@@ -2,6 +2,12 @@
   console.log(`form working`);
 }());
 
-$(`.info-form`).on(`submit`, function () {
-  
+$(`form`).on(`submit`, function (event) {
+  event.preventDefault()
+  $.ajax({
+    type: 'POST',
+    url: 'https://galvanize-eats-api.herokuapp.com/orders'
+  }).done(function(data){
+    console.log(data);
+  })
 })
